19b81db4a8f05776d0b56ab8ba3c2273b6c4d957
apatchy <keco98@gmail.com>
2026-04-02 19:12:06 -0500

    Clicking link now fails minor adjustments

--- submission/main.js
+++ submission/main.js
@@ -1,10 +1,5 @@
 import { CircularBuffer } from "./circular-buffer.js";
 
-const overlayContainer = document.getElementById("overlay-container");
-const instructionContainer = document.getElementById("instruction-container")
-const failContainer = document.getElementById("fail-container")
-
-
 // CPS -> Clicks Per Second
 // PBR -> Playback Rate
 
@@ -24,16 +19,23 @@ const MAX_CPS = Math.max(12, Math.sqrt(MAX_PBR) * UNIT_CPS)
 
 // CPS when clicks are first tracked 
 // 0 -> Instantly show overlay message and pause video
-// UNIT_CPS -> PBR will start normal
+// UNIT_CPS -> PBR will start normal and slow down gradually
 const INITIAL_CPS = UNIT_CPS
 
-// Buffer size for running average of CPS (larger = slower response time)
+// Buffer size for running average of CPS 
+// larger -> slower response time
 const BUFFER_SIZE = 100
 
 // Can fail ad by not clicking for a period of time (seconds)
 const CAN_FAIL = true
 const FAIL_TIME = 10
+// How long fail message is shown before posting fail event
+const FAIL_MESSAGE_TIME = 1
 
+const overlayContainer = document.getElementById("overlay-container");
+const instructionContainer = document.getElementById("instruction-container")
+const failContainer = document.getElementById("fail-container")
+const linkElement = document.getElementById("link")
 
 let buffer = new CircularBuffer(BUFFER_SIZE)
 let clickTimestamps = [];
@@ -49,6 +51,11 @@ window.addEventListener('message', (event) => {
             setTimeout(() => {
                 overlayContainer.addEventListener("click", (e) => {
                     clickTimestamps.push(Date.now());
+                })
+
+                linkElement.addEventListener('click', (e) => {
+                    cancelAnimationFrame(animationFrameID)
+                    window.top.postMessage({ type: 'fail' }, '*');
                 })
                 buffer.fill(INITIAL_CPS)
                 averageCPS = INITIAL_CPS
@@ -101,7 +108,7 @@ function update(timestamp) {
                 cancelAnimationFrame(animationFrameID)
                 setTimeout(() => {
                     window.top.postMessage({ type: 'fail' }, '*');
-                }, 3000);
+                }, FAIL_MESSAGE_TIME * 1000);
                 return;
             }
         }
@@ -109,7 +116,8 @@ function update(timestamp) {
     animationFrameID = requestAnimationFrame(update)
 }
 
-//  Uses quatradic equation intersecting the three points (0,0), (UNIT_CPS, 1.0), and (MAX_CPS, MAX_PBR).
+//  Uses quadratic equation intersecting the three points (0,0), (UNIT_CPS, 1.0), and (MAX_CPS, MAX_PBR)
+//  to interpolate PBR as a function of CPS
 function calculatePBR(cps) {
     const d = UNIT_CPS * MAX_CPS * (MAX_CPS - UNIT_CPS)
     const a = (MAX_PBR * UNIT_CPS - MAX_CPS) / d
